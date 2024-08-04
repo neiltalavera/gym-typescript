2a9037d675adbd4d7829b11587065fb565523751
neiltalavera <lien.talavera@gmail.com>
2024-08-04 16:30:20 +0800

    remove props

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,12 +1,6 @@
-import { SelectedPage } from '@/shared/types';
-import React from 'react'
 import Logo from '@/assets/Logo.png'
 
-type Props = {
-    setSelectedPage: (value: SelectedPage) => void;
-}
-
-const Footer = ({ setSelectedPage }: Props) => {
+const Footer = () => {
   return (
     <footer className='bg-primary-100 py-16'>
         <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
